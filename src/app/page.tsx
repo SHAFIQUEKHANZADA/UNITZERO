f4e3f5d7490e442461e93d683347e08e57133160
shafique <shafique@sleepwellsystem.com>
2025-08-30 22:36:47 +0500

    Added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Hero from "@/components/Hero";
 import HowWeWorkTimeline from "@/components/Offer";
 import ScrollingBar from "@/components/Scrollbar";
 import Services from "@/components/Service";
-import Teams from "@/components/Teams";
+// import Teams from "@/components/Teams";
 
 export default function Home() {
   return (
@@ -14,7 +14,7 @@ export default function Home() {
       <Services/>
       <ScrollingBar/> 
       <HowWeWorkTimeline/>
-      <Teams/>
+      {/* <Teams/> */}
       <Contact/>
     </div>
   );
